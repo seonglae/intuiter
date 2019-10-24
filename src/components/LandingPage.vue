8b122936b06fa372241f09bac613e091a33aecad
seonglaecho <sungle3737@gmail.com>
2019-10-25 00:02:06 +0900

    favicon edti

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,31 +1,16 @@
 <template>
   <v-container>
-    <v-layout
-      text-center
-      wrap
-    >
+    <v-layout text-center wrap>
       <v-flex xs12>
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        ></v-img>
+        <v-img :src="require('../assets/logo.svg')" class="my-3" contain height="200"></v-img>
       </v-flex>
 
       <v-flex mb-4>
-        <h1 class="display-2 font-weight-bold mb-3">
-          Intuit Manager
-        </h1>
-        <v-btn
-            v-on:click="make"
-          >Make and Run</v-btn>
+        <h1 class="display-2 font-weight-bold mb-3">Intuit Manager</h1>
+        <v-btn v-on:click="make">Make and Run</v-btn>
       </v-flex>
 
-      <v-flex
-        mb-5
-        xs12
-      >
+      <v-flex mb-5 xs12>
         <h2 class="headline font-weight-bold mb-3">What's next?</h2>
 
         <v-layout justify-center>
@@ -35,16 +20,11 @@
             :href="next.href"
             class="subheading mx-3"
             target="_blank"
-          >
-            {{ next.text }}
-          </a>
+          >{{ next.text }}</a>
         </v-layout>
       </v-flex>
 
-      <v-flex
-        xs12
-        mb-5
-      >
+      <v-flex xs12 mb-5>
         <h2 class="headline font-weight-bold mb-3">Important Links</h2>
 
         <v-layout justify-center>
@@ -54,16 +34,11 @@
             :href="link.href"
             class="subheading mx-3"
             target="_blank"
-          >
-            {{ link.text }}
-          </a>
+          >{{ link.text }}</a>
         </v-layout>
       </v-flex>
 
-      <v-flex
-        xs12
-        mb-5
-      >
+      <v-flex xs12 mb-5>
         <h2 class="headline font-weight-bold mb-3">Ecosystem</h2>
 
         <v-layout justify-center>
@@ -73,9 +48,7 @@
             :href="eco.href"
             class="subheading mx-3"
             target="_blank"
-          >
-            {{ eco.text }}
-          </a>
+          >{{ eco.text }}</a>
         </v-layout>
       </v-flex>
     </v-layout>
@@ -87,62 +60,91 @@ export default {
   data: () => ({
     ecosystem: [
       {
-        text: 'vuetify-loader',
-        href: 'https://github.com/vuetifyjs/vuetify-loader',
+        text: "vuetify-loader",
+        href: "https://github.com/vuetifyjs/vuetify-loader"
       },
       {
-        text: 'github',
-        href: 'https://github.com/vuetifyjs/vuetify',
+        text: "github",
+        href: "https://github.com/vuetifyjs/vuetify"
       },
       {
-        text: 'awesome-vuetify',
-        href: 'https://github.com/vuetifyjs/awesome-vuetify',
-      },
+        text: "awesome-vuetify",
+        href: "https://github.com/vuetifyjs/awesome-vuetify"
+      }
     ],
     importantLinks: [
       {
-        text: 'Documentation',
-        href: 'https://vuetifyjs.com',
+        text: "Documentation",
+        href: "https://vuetifyjs.com"
       },
       {
-        text: 'Chat',
-        href: 'https://community.vuetifyjs.com',
+        text: "Chat",
+        href: "https://community.vuetifyjs.com"
       },
       {
-        text: 'Made with Vuetify',
-        href: 'https://madewithvuejs.com/vuetify',
+        text: "Made with Vuetify",
+        href: "https://madewithvuejs.com/vuetify"
       },
       {
-        text: 'Twitter',
-        href: 'https://twitter.com/vuetifyjs',
+        text: "Twitter",
+        href: "https://twitter.com/vuetifyjs"
       },
       {
-        text: 'Articles',
-        href: 'https://medium.com/vuetify',
-      },
+        text: "Articles",
+        href: "https://medium.com/vuetify"
+      }
     ],
     whatsNext: [
       {
-        text: 'Explore components',
-        href: 'https://vuetifyjs.com/components/api-explorer',
+        text: "Explore components",
+        href: "https://vuetifyjs.com/components/api-explorer"
       },
       {
-        text: 'Select a layout',
-        href: 'https://vuetifyjs.com/layout/pre-defined',
+        text: "Select a layout",
+        href: "https://vuetifyjs.com/layout/pre-defined"
       },
       {
-        text: 'Frequently Asked Questions',
-        href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-      },
-    ],
+        text: "Frequently Asked Questions",
+        href: "https://vuetifyjs.com/getting-started/frequently-asked-questions"
+      }
+    ]
   }),
   methods: {
-    make () {
-      var make = require('node-cmd')
-      make.run('"resources\\ahk\\exe\\ahk\\AutoHotkey.exe" "resources\\ahk\\make.ahk"')
-      const remote = require('electron').remote
-      let win = remote.getCurrentWindow()
-      win.close()
+    make() {
+      var path = require("electron").remote.app.getAppPath();
+      console.log(path);
+
+      // command run
+      var cmd = require("node-cmd");
+      cmd.get(
+        '"resources\\ahk\\exe\\ahk\\AutoHotkey.exe" "resources\\ahk\\make.ahk"',
+        function(err, data, stderr) {
+          // close window
+          if (!err) {
+            const remote = require("electron").remote;
+            let win = remote.getCurrentWindow();
+            win.close();
+          } else {
+            const { dialog } = require('electron')
+
+            const options = {
+              type: "error",
+              title: "Error",
+              message: "Some Error Occured",
+              detail: "please press make button one more time"
+            };
+
+            dialog.showMessageBox(
+              null,
+              options,
+              (response) => {
+                console.log(response);
+              }
+            );
+            console.log("error", err, data, stderr);
+          }
+        }
+      );
     }
   }
 };
