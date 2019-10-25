71ae487503514d1881451882b97cbacce8a1e332
seonglaecho <sungle3737@gmail.com>
2019-10-25 23:08:34 +0900

    readme

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,13 +1,13 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib';
+import Vue from 'vue'
+import Vuetify from 'vuetify/lib'
 
-Vue.use(Vuetify);
+Vue.use(Vuetify)
 
 export default new Vuetify({
   icons: {
-    iconfont: 'mdi',
+    iconfont: 'mdi'
   },
   theme: {
-    dark: true,
+    dark: true
   }
-});
+})
