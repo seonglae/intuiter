21dba28339d55a432e2dd8b3080b1ff7700722c0
seonglaecho <sungle3737@gmail.com>
2019-11-10 17:50:14 +0900

    😎0.2.0 beta

--- vue.config.js
+++ vue.config.js
@@ -16,6 +16,7 @@ module.exports = {
   pluginOptions: {
     electronBuilder: {
       builderOptions: {
+        "generateUpdatesFilesForAllChannels": true,
         extraResources: [
           {
             from: './resources/ahk/',
@@ -28,18 +29,18 @@ module.exports = {
         nsis: {
           deleteAppDataOnUninstall: true,
           createDesktopShortcut: true,
-          include: 'dist_electron/nsis/remove_startup.nsh'
+          include: 'dist/nsis/remove_startup.nsh',
         },
         appx: {
           identityName: 'sungleCho.Intuiter',
           applicationId: 'Intuiter',
           publisher: 'CN=169E7280-3382-426D-AE3B-5FB3786A703F',
-          publisherDisplayName: 'seonglae'
+          publisherDisplayName: 'seonglae',
         },
         productName: 'Intuiter',
         appId: 'com.seonglae.intuiter',
         directories: {
-          output: 'dist_electron'
+          output: 'dist'
         },
         win: {
           target: [
@@ -47,9 +48,9 @@ module.exports = {
             'appx'
           ],
           requestedExecutionLevel: 'highestAvailable',
-          certificateFile: 'dist_electron/Seonglae.pfx',
+          certificateFile: 'dist/Seonglae.pfx',
           certificatePassword: 'sung1103215!',
-          icon: 'dist_electron/icons/icon.ico',
+          icon: 'dist/icons/icon.ico',
           certificateSubjectName: 'CN=developmentca',
           rfc3161TimeStampServer: 'http://timestamp.globalsign.com/scripts/timstamp.dll'
         }
