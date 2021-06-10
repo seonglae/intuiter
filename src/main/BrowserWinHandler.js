b29a44794425075047b1c8d6a40cc30c88a8042b
seonglae <sungle3737@gmail.com>
2021-06-10 11:44:21 +0900

    Before resolve electron error

--- src/main/BrowserWinHandler.js
+++ src/main/BrowserWinHandler.js
@@ -1,7 +1,10 @@
+/* eslint-disable */
 import { EventEmitter } from 'events'
 import { BrowserWindow, app } from 'electron'
-const isProduction = process.env.NODE_ENV === 'production'
 
+const DEV_SERVER_URL = process.env.DEV_SERVER_URL
+const isProduction = process.env.NODE_ENV === 'production'
+const isDev = process.env.NODE_ENV === 'development'
 export default class BrowserWinHandler {
   /**
    * @param [options] {object} - browser window options
@@ -16,15 +19,12 @@ export default class BrowserWinHandler {
   }
 
   _createInstance() {
-    // This method will be called when Electron has finished
-    // initialization and is ready to create browser windows.
-    // Some APIs can only be used after this event occurs.
-    app.on('ready', () => {
-      this._create()
-    })
-
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
+    if (app.isReady()) this._create()
+    else {
+      app.once('ready', () => {
+        this._create()
+      })
+    }
     if (!this.allowRecreate) return
     app.on('activate', () => this._recreate())
   }
@@ -34,13 +34,12 @@ export default class BrowserWinHandler {
       ...this.options,
       webPreferences: {
         ...this.options.webPreferences,
-        webSecurity: isProduction, // disable on dev to allow loading local resources
-        nodeIntegration: true, // allow loading modules via the require () function
-        devTools: !process.env.SPECTRON, // disable on e2e test environment
-      },
+        webSecurity: isProduction,
+        nodeIntegration: true,
+        contextIsolation: false
+      }
     })
     this.browserWindow.on('closed', () => {
-      // Dereference the window object
       this.browserWindow = null
     })
     this._eventEmitter.emit('created')
@@ -51,18 +50,21 @@ export default class BrowserWinHandler {
   }
 
   /**
-   * @callback onReadyCallback
-   * @param {BrowserWindow}
-   */
-
-  /**
    *
    * @param callback {onReadyCallback}
    */
   onCreated(callback) {
+    if (this.browserWindow !== null) return callback(this.browserWindow)
     this._eventEmitter.once('created', () => {
       callback(this.browserWindow)
     })
+  }
+
+  async loadPage(pagePath) {
+    if (!this.browserWindow) return Promise.reject(new Error("The page could not be loaded before win 'created' event"))
+    const serverUrl = isDev ? DEV_SERVER_URL : 'app://./index.html'
+    const fullPath = serverUrl + '#' + pagePath
+    await this.browserWindow.loadURL(fullPath)
   }
 
   /**
@@ -71,9 +73,7 @@ export default class BrowserWinHandler {
    */
   created() {
     return new Promise(resolve => {
-      this._eventEmitter.once('created', () => {
-        resolve(this.browserWindow)
-      })
+      this.onCreated(() => resolve(this.browserWindow))
     })
   }
 }
--- src/renderer/nuxt.config.js
+++ src/renderer/nuxt.config.js
@@ -1,28 +1,15 @@
-/**
- * By default, Nuxt.js is configured to cover most use cases.
- * This default configuration can be overwritten in this file
- * @link {https://nuxtjs.org/guide/configuration/}
- */
-
 module.exports = {
+  ssr: false,
+  target: 'static',
   head: {
-    title: 'robota',
-    meta: [{ charset: 'utf-8' }],
+    title: 'intuiter',
+    meta: [{ charset: 'utf-8' }]
   },
   loading: false,
-  plugins: [{ ssr: true, src: '@/plugins/icons.js' }],
-  buildModules: ['@nuxt/typescript-build'],
+  plugins: ['@/plugins/icons.js'],
+  buildModules: ['@nuxt/typescript-build', '@nuxtjs/composition-api/module'],
   modules: ['@nuxtjs/vuetify'],
   vuetify: {
-    theme: {
-      themes: {
-        light: {
-          primary: '#1867c0',
-          secondary: '#b0bec5',
-          accent: '#8c9eff',
-          error: '#b71c1c',
-        },
-      },
-    },
-  },
+    theme: { dark: true }
+  }
 }
