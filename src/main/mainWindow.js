d6f5a270f3b18648693ea8a5534a3455825c9de6
seonglae <sungle3737@gmail.com>
2021-06-10 11:44:21 +0900

    Before resolve electron error

--- src/main/mainWindow.js
+++ src/main/mainWindow.js
@@ -1,18 +1,15 @@
-import path from 'path'
 import BrowserWinHandler from './BrowserWinHandler'
-const isDev = process.env.NODE_ENV === 'development'
-
-const INDEX_PATH = path.join(__dirname, '..', 'renderer', 'index.html')
-const DEV_SERVER_URL = process.env.DEV_SERVER_URL // eslint-disable-line prefer-destructuring
 
 const winHandler = new BrowserWinHandler({
-  height: 600,
-  width: 1000,
+  width: 850,
+  height: 500,
+  minWidth: 850,
+  minHeight: 500,
+  transparent: true,
+  frame: false,
+  titleBarStyle: 'hiddenInset'
 })
 
-winHandler.onCreated(browserWindow => {
-  if (isDev) browserWindow.loadURL(DEV_SERVER_URL)
-  else browserWindow.loadFile(INDEX_PATH)
-})
+winHandler.onCreated(_browserWindow => winHandler.loadPage('/'))
 
 export default winHandler
