93b94de122774943f653986fd5588db99ab4adf2
seonglae <sungle3737@gmail.com>
2021-06-16 15:01:55 +0900

    Working tabs

--- src/renderer/pages/index.vue
+++ src/renderer/pages/index.vue
@@ -1,11 +1,17 @@
 <template>
   <v-app>
     <v-app-bar app style="-webkit-app-region: drag">
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Intuiter - </span>
-        <span class="font-weight-light">Intuit Manager</span>
-      </v-toolbar-title>
-      <v-spacer />
+      <v-app-bar-title class="headline text-uppercase">
+        <span>Intuit </span>
+        <span class="font-weight-light">Manager</span>
+      </v-app-bar-title>
+
+      <v-tabs v-model="index" style="-webkit-app-region: no-drag">
+        <v-spacer style="-webkit-app-region: drag" />
+        <v-tab v-for="tab in tabs" :key="tab.type"> {{ tab.name }} </v-tab>
+        <v-spacer style="-webkit-app-region: drag" />
+      </v-tabs>
+
       <v-btn v-on:click="openDoc" class="mr-2">
         Doc
       </v-btn>
@@ -13,6 +19,7 @@
         Exit
       </v-btn>
     </v-app-bar>
+
     <v-main>
       <make-space />
     </v-main>
@@ -21,7 +28,7 @@
 
 <script lang="ts">
 import MakeSpace from '@/components/MakeSpace.vue'
-import { defineComponent } from '@vue/composition-api'
+import { defineComponent, ref } from '@vue/composition-api'
 import { shell, remote } from 'electron'
 
 const DOC = 'https://github.com/seonglae/intuiter'
@@ -31,7 +38,15 @@ export default defineComponent({
   components: { MakeSpace },
   setup() {
     const openDoc = () => shell.openExternal(DOC)
-    return { openDoc, remote }
+
+    const tabs = [
+      { name: 'Home', key: 'home' },
+      { name: 'Option', key: 'opt' },
+      { name: 'Shortcut', key: 'shortcut' },
+      { name: 'Extension', key: 'ext' }
+    ]
+    const index = ref(0)
+    return { openDoc, remote, tabs, index }
   }
 })
 </script>
