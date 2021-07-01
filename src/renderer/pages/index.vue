76f474b6c7c1270911563e9053a6bbf356c72654
seonglae <sungle3737@gmail.com>
2021-07-01 20:47:14 +0900

    feat: bottom bigger round version

--- src/renderer/pages/index.vue
+++ src/renderer/pages/index.vue
@@ -1,19 +1,15 @@
 <template>
-  <v-app
-    style="
-  border-radius: 10px;
-  overflow: hidden;"
-  >
-    <v-app-bar app style="-webkit-app-region: drag;  border-radius: 10px; overflow: hidden;">
+  <v-app id="app">
+    <v-app-bar app id="bar" class="top">
       <v-app-bar-title class="headline text-uppercase">
         <span>Intuit </span>
         <span class="font-weight-light">Manager</span>
       </v-app-bar-title>
 
-      <v-tabs v-model="index" style="-webkit-app-region: no-drag">
-        <v-spacer style="-webkit-app-region: drag" />
+      <v-tabs v-model="index" class="nodrag">
+        <v-spacer class="drag" />
         <v-tab v-for="tab in tabs" :key="tab.type"> {{ tab.name }} </v-tab>
-        <v-spacer style="-webkit-app-region: drag" />
+        <v-spacer class="drag" />
       </v-tabs>
 
       <v-btn v-on:click="openDoc" class="mr-2">
@@ -24,14 +20,12 @@
       </v-btn>
     </v-app-bar>
 
-    <v-main> </v-main>
-
-    <v-tabs-items v-model="index" class="wrapper" style="background: #121213" touchless>
-      <v-tab-item v-for="(tab, i) in tabs" :key="i" eager>
-        <templates-run v-if="tab.key === 'home'" />
-        <templates-opts v-else-if="tab.key === 'opt'" />
-        <templates-shortcuts v-else-if="tab.key === 'shortcut'" />
-        <templates-exts v-else-if="tab.key === 'ext'" />
+    <v-tabs-items v-model="index" class="wrapper bottom" id="view" touchless>
+      <v-tab-item v-for="(tab, i) in tabs" :key="i" eager class="wrapper">
+        <templates-run v-if="tab.key === 'home'" class="wrapper" />
+        <templates-opts v-else-if="tab.key === 'opt'" class="wrapper" />
+        <templates-shortcuts v-else-if="tab.key === 'shortcut'" class="wrapper" />
+        <templates-exts v-else-if="tab.key === 'ext'" class="wrapper" />
       </v-tab-item>
     </v-tabs-items>
   </v-app>
@@ -60,11 +54,4 @@ export default defineComponent({
 })
 </script>
 
-<style>
-::-webkit-scrollbar {
-  display: none;
-}
-button {
-  -webkit-app-region: no-drag;
-}
-</style>
+<style src="~/assets/style.css" />
