0c890b5bdcb2f1503a5da271a3c560757875dded
Restyled.io <commits@restyled.io>
2021-07-04 14:43:10 +0000

    Restyled by prettier

--- src/renderer/nuxt.config.js
+++ src/renderer/nuxt.config.js
@@ -2,13 +2,13 @@
 
 /** @type {import('@nuxt/types').NuxtOptions} */
 module.exports = {
-  ssr : false,
-  target : 'static',
-  head : {title : 'intuiter', meta : [ {charset : 'utf-8'} ]},
-  components : true,
-  loading : false,
-  buildModules : [ '@nuxt/typescript-build', '@nuxtjs/composition-api/module' ],
-  modules : [ '@nuxtjs/vuetify', '@nuxtjs/toast' ],
-  toast : {position : 'bottom-center', duration : 2000, keepOnHover : true},
-  vuetify : {theme : {dark : true}}
+  ssr: false,
+  target: 'static',
+  head: { title: 'intuiter', meta: [{ charset: 'utf-8' }] },
+  components: true,
+  loading: false,
+  buildModules: ['@nuxt/typescript-build', '@nuxtjs/composition-api/module'],
+  modules: ['@nuxtjs/vuetify', '@nuxtjs/toast'],
+  toast: { position: 'bottom-center', duration: 2000, keepOnHover: true },
+  vuetify: { theme: { dark: true } },
 }
